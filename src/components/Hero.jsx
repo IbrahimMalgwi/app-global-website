77089e2e15aa0017af7a3570a2ac314318e67e53
IbrahimMalgwi <ibrahimganamalgwi@outlook.com>
2025-08-28 11:04:41 +0100

    updated hero page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const slides = [
         title: (
             <>
         <span className="bg-gradient-to-r from-green-600 via-blue-600 to-violet-600 bg-clip-text text-transparent">
-          Transforming Healthcare.
+          Transforming Healthcare
         </span>
                 <br />
                 <span className="bg-gradient-to-r from-violet-600 via-green-600 to-blue-600 bg-clip-text text-transparent">
